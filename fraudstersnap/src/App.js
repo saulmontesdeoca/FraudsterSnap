49bbe63efe62b752610de39e1ae1b208e9c3e221
vcoetoG <vcoeto@hotmail.com>
2021-04-29 22:48:42 -0500

    added login

--- fraudstersnap/src/App.js
+++ fraudstersnap/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+  const respuestaGoogle=(respuesta)=> {
+    console.log(respuesta);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={respuestaGoogle}
+    onFailure={respuestaGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
     </div>
   );
 }
