56953028a7a1a3211376aa9c3920583807ff14ef
Diego Acevedo <diegomx2@hotmail.com>
2021-04-30 14:53:54 -0500

    server

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,50 @@
 # Import libraries
+import csv 
+import json 
 import numpy as np
 from flask import Flask, request, jsonify
 import pickle
-app = Flask(__name__)
+app = Flask(__name__, template_folder="template")
+
+def open_json():
+    with open('test.json', 'r') as j:
+        json_data = json.load(j)
+        print(json_data)
+        return json_data
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
 # Load the model
 model = pickle.load(open('modelo.pkl','rb'))
 @app.route('/api',methods=['POST'])
 def predict():
     # Get the data from the POST request.
     data = request.get_json(force=True)
+    print(data)
     # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
+
+    # csvFilePath = r'data.csv'
+    # jsonFilePath = r'data.json'
+    # csv_to_json(csvFilePath, jsonFilePath)
+
+    json_data = open_json()
+    prediction = model.predict([np.array(json_data)])
     # Take the first value of prediction
     output = prediction[0]
     return jsonify(output)
