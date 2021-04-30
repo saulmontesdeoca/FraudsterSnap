dded0536124716083559652394aa90266462ee43
vcoetoG <vcoeto@hotmail.com>
2021-04-30 00:06:51 -0500

    added firbase auth

--- fraudstersnap/src/App.js
+++ fraudstersnap/src/App.js
@@ -1,21 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import GoogleLogin from 'react-google-login';
+import Login from './login.js';
 
 function App() {
-  const respuestaGoogle=(respuesta)=> {
-    console.log(respuesta);
-  }
   return (
     <div className="App">
-      <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={respuestaGoogle}
-    onFailure={respuestaGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-  {/* document.getElementById('googleButton') */}
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+          <Login />
+      </header>
     </div>
   );
 }
