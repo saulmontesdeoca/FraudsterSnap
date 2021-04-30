4a7927896bf6861d7830b986fe60ecd47934c7e4
vcoetoG <vcoeto@hotmail.com>
2021-04-29 22:50:34 -0500

    updated login button

--- fraudstersnap/src/App.js
+++ fraudstersnap/src/App.js
@@ -14,8 +14,8 @@ function App() {
     onSuccess={respuestaGoogle}
     onFailure={respuestaGoogle}
     cookiePolicy={'single_host_origin'}
-  />,
-  document.getElementById('googleButton')
+  />
+  {/* document.getElementById('googleButton') */}
     </div>
   );
 }
